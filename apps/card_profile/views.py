2c5ef77679b1e29fce60d140e92d4266cc730bd1
Nova Card <getnovacard@gmail.com>
2021-10-04 11:42:31 +0000

    removed debug helpers from card_profile edit_profile view

--- apps/card_profile/views.py
+++ apps/card_profile/views.py
@@ -20,29 +20,17 @@ def view_profile(request, username):
 
 @login_required
 def edit_profile(request, username):
-    print(1)
     user_profile = get_object_or_404(Card_profile, username=username)
 
     if request.method == 'POST':
         form = EditProfile(request.POST, request.FILES, instance=user_profile)
-        print(2)
-        x = form.is_valid()
-        print(3)
-        y = form.errors
-        print(4)
-        print(form.errors)
 
-        print(5)
         if form.is_valid():
-            print(6)
             form.save()
 
-            print(7)
             return redirect('view_profile', username=user_profile.username)
 
     else:
-        print(8)
         form = EditProfile()
 
-    print(9)
     return render(request, 'card_profile/edit_profile.html', {'user_profile': user_profile, 'form': form})
